234c1414a3518810b48c22251aff1beac7e62268
jasontbradshaw <jasontbradshaw@f6f5931a-53a0-11de-b413-c5e99dc0a8e2>
2009-06-08 20:27:57 +0000

    Changed from 2-space to 4-space indent to match coding standards.

--- trunk/coconut_run.py
+++ trunk/coconut_run.py
@@ -3,73 +3,73 @@ from sprites import Block, Avatar
 pygame.init()
 
 def main(argv=None):
-  """ main game loop """
-  if argv is None:
-    argv = sys.argv
+    """ main game loop """
+    if argv is None:
+        argv = sys.argv
 
-  resource_folder = 'resources/'
-  bg_file = resource_folder + 'landscape.bmp'
-  avatar_file = resource_folder + 'avatar.bmp'
-  block_file = resource_folder + 'ice.bmp'
-  COLORKEY = 0xff00ff         # transparent color
+    resource_folder = 'resources/'
+    bg_file = resource_folder + 'landscape.bmp'
+    avatar_file = resource_folder + 'avatar.bmp'
+    block_file = resource_folder + 'ice.bmp'
+    COLORKEY = 0xff00ff                 # transparent color
 
-  # GUI window
-  size = width, height = 640, 480
-  screen = pygame.display.set_mode(size)
+    # GUI window
+    size = width, height = 640, 480
+    screen = pygame.display.set_mode(size)
 
-  # keyboard delay before key repeats
-  pygame.key.set_repeat(10, 10)
+    # keyboard delay before key repeats
+    pygame.key.set_repeat(10, 10)
 
-  # load and set up background
-  bg = pygame.image.load(bg_file).convert()
-  bg_rect = bg.get_rect()
+    # load and set up background
+    bg = pygame.image.load(bg_file).convert()
+    bg_rect = bg.get_rect()
 
-  # load and set up avatar
-  avatar_surf = pygame.image.load(avatar_file).convert()
-  avatar_surf.set_colorkey(COLORKEY)
-  avatar_rect = avatar_surf.get_rect()
-  avatar_vel = 10
-  avatar = Avatar(avatar_surf,
-      [width/2, height - avatar_rect.height],
-      avatar_vel)
+    # load and set up avatar
+    avatar_surf = pygame.image.load(avatar_file).convert()
+    avatar_surf.set_colorkey(COLORKEY)
+    avatar_rect = avatar_surf.get_rect()
+    avatar_vel = 10
+    avatar = Avatar(avatar_surf,
+            [width/2, height - avatar_rect.height],
+            avatar_vel)
 
-  # blocks (currently just testing one block)
-  block_surf = pygame.image.load(block_file).convert()
-  block_surf.set_colorkey(COLORKEY)
-  blocks = pygame.sprite.Group()
-  # make some test blocks
-  blocks.add(Block(block_surf, [100, 0], 2, height))
-  blocks.add(Block(block_surf, [400, 0], 4, height))
+    # blocks (currently just testing one block)
+    block_surf = pygame.image.load(block_file).convert()
+    block_surf.set_colorkey(COLORKEY)
+    blocks = pygame.sprite.Group()
+    # make some test blocks
+    blocks.add(Block(block_surf, [100, 0], 2, height))
+    blocks.add(Block(block_surf, [400, 0], 4, height))
 
-  while 1:
-    # input handling
-    for event in pygame.event.get():
-      if event.type == pygame.QUIT: sys.exit()
-      elif event.type == pygame.KEYDOWN:
-        if event.key == pygame.K_LEFT:
-          if not avatar.too_left():
-            avatar.update(-1)
-        elif event.key == pygame.K_RIGHT:
-          if not avatar.too_right(width):
-            avatar.update(1)
-        elif event.key == pygame.K_ESCAPE:
-          sys.exit()
+    while 1:
+        # input handling
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    if not avatar.too_left():
+                        avatar.update(-1)
+                elif event.key == pygame.K_RIGHT:
+                    if not avatar.too_right(width):
+                        avatar.update(1)
+                elif event.key == pygame.K_ESCAPE:
+                    sys.exit()
 
-    # update sprites
-    blocks.update() 
+        # update sprites
+        blocks.update() 
 
-    # calculate state conditions
+        # calculate state conditions
 
-    # update game state
+        # update game state
 
-    # redraw
-    screen.blit(bg, bg_rect)
-    screen.blit(avatar.image, avatar.rect)
-    for b in blocks:
-      screen.blit(b.image, b.rect)
-    pygame.display.flip()
-    #pygame.time.delay(10)
+        # redraw
+        screen.blit(bg, bg_rect)
+        screen.blit(avatar.image, avatar.rect)
+        for b in blocks:
+            screen.blit(b.image, b.rect)
+        pygame.display.flip()
+        #pygame.time.delay(10)
 
 if __name__ == "__main__":
-    sys.exit(main())
+        sys.exit(main())
 
