46597aed96c59d8ba6914e74e7b3a07630ee6c35
jasontbradshaw <jasontbradshaw@f6f5931a-53a0-11de-b413-c5e99dc0a8e2>
2009-06-08 20:27:57 +0000

    Changed from 2-space to 4-space indent to match coding standards.

--- sprites.py
+++ sprites.py
@@ -1,61 +1,61 @@
 import pygame
 
 class Avatar(pygame.sprite.Sprite):
-  def __init__(self, surface, initial_position, vel = 0):
-    pygame.sprite.Sprite.__init__(self)
-    self.image = surface
-    self.rect = self.image.get_rect()
-    self.rect.topleft = initial_position
-    self.vel = vel
+    def __init__(self, surface, initial_position, vel = 0):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = surface
+        self.rect = self.image.get_rect()
+        self.rect.topleft = initial_position
+        self.vel = vel
 
-  def update(self, dir):
-    """
-    If dir > 0, move avatar right by self.vel pixels.
-    If dir < 0, move avatar left.
-    """
-    
-    if dir > 0:
-      self.rect = self.rect.move(self.vel, 0)
-    else:
-      self.rect = self.rect.move(-self.vel, 0)
+    def update(self, dir):
+        """
+        If dir > 0, move avatar right by self.vel pixels.
+        If dir < 0, move avatar left.
+        """
+        
+        if dir > 0:
+            self.rect = self.rect.move(self.vel, 0)
+        else:
+            self.rect = self.rect.move(-self.vel, 0)
 
-  def set_vel(self, v):
-    self.vel = vel
+    def set_vel(self, v):
+        self.vel = vel
 
-  def valid_pos(self, width):
-    if too_left(self) or too_right(self):
-      return False
-    return True
+    def valid_pos(self, width):
+        if too_left(self) or too_right(self):
+            return False
+        return True
 
-  def too_left(self):
-    """Returns True if avatar is too far to the left of screen"""
-    if self.rect.left <= 0: return True
-    return False
+    def too_left(self):
+        """Returns True if avatar is too far to the left of screen"""
+        if self.rect.left <= 0: return True
+        return False
  
-  def too_right(self, width):
-    """Returns True if avatar is too far to the right of screen"""
-    if self.rect.right >= width: return True
-    return False
+    def too_right(self, width):
+        """Returns True if avatar is too far to the right of screen"""
+        if self.rect.right >= width: return True
+        return False
 
 class Block(pygame.sprite.Sprite):
 
-  def __init__(self, surface, initial_position, vel = 0, ground_lvl = 0):
-    pygame.sprite.Sprite.__init__(self)
-    self.image = surface
-    self.rect = self.image.get_rect()
-    self.rect.topleft = initial_position
-    self.vel = vel
-    self.ground_lvl = ground_lvl 
+    def __init__(self, surface, initial_position, vel = 0, ground_lvl = 0):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = surface
+        self.rect = self.image.get_rect()
+        self.rect.topleft = initial_position
+        self.vel = vel
+        self.ground_lvl = ground_lvl 
 
-  def update(self):
-    """moves block down by self.vel pixels"""
-    if not self.on_ground():
-      self.rect = self.rect.move(0, self.vel)
+    def update(self):
+        """moves block down by self.vel pixels"""
+        if not self.on_ground():
+            self.rect = self.rect.move(0, self.vel)
 
-  def on_ground(self):
-    """returns True if block is on the ground"""
-    if self.ground_lvl - self.rect.top - self.rect.height == 0:
-      return True
-    return False
+    def on_ground(self):
+        """returns True if block is on the ground"""
+        if self.ground_lvl - self.rect.top - self.rect.height == 0:
+            return True
+        return False
 
 
