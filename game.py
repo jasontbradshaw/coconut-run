dbf36cd0b17bd19ea221c0b8d54de32beabb9193
jasontbradshaw <jasontbradshaw@f6f5931a-53a0-11de-b413-c5e99dc0a8e2>
2009-06-09 19:54:41 +0000

    abstracted width/height/etc. into Level class, removed block tests (superseded by random generation)

--- game.py
+++ game.py
@@ -8,26 +8,31 @@ class Game:
 
 class Level:
     """Defines level settings such as difficulty."""
-    def __init__(self, lvl_name = "Cococana", bg_file="landscape.bmp",
+    
+    def __init__(self, lvl_name = "Level X", bg_file="landscape.bmp",
                  time_limit = 120, max_blocks = -1,
                  min_vel = 2, max_vel = 10,
-                 left = 0, right = 640,
+                 left = 0, top = 0, right = 640, bottom = 480,
                  items_dropped = [True, False]):
+        
         self.lvl_name = lvl_name
-
+        
+        # level size
+        self.left = left
+        self.right = right
+        self.top = top
+        self.bottom = bottom
+        
         # build background image and rect
         self.bg_file = bg_file
         self.bg_image = pygame.image.load("resources/" + bg_file).convert()
         self.bg_rect = self.bg_image.get_rect()
-
+        
         self.time_limit = time_limit    # seconds; -1 = infinity
         self.max_blocks = max_blocks    # -1 = infinity
         self.min_vel = min_vel          # velocity blocks fall
         self.max_vel = max_vel
         
-        self.left_cutoff = left
-        self.right_cutoff = right
-
         # items possibly dropped
         # [coconuts, bananas, ...]
         self.items_dropped = items_dropped
