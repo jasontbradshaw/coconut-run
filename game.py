a149b05aa1073b61094479b42c4358d9f3c03654
elbenshira <elbenshira@f6f5931a-53a0-11de-b413-c5e99dc0a8e2>
2009-06-14 19:03:39 +0000

    removed Level.max_blocks usage; put block fall frequency into Level; moved screen draw to beginning of main loop to prevent displaying level after game over screen

--- game.py
+++ game.py
@@ -4,7 +4,8 @@ class Level:
     """Defines level settings such as difficulty."""
     
     def __init__(self, name = "Default", bg_file="landscape.bmp",
-                 time_limit = 120, max_blocks = -1,
+                 time_limit = 120,
+                 block_freq = .015, block_freq_inc = 0.00001,
                  min_vel = 2, max_vel = 10,
                  left = 0, top = 0, right = 640, bottom = 480,
                  items_dropped = [True, False]):
@@ -23,9 +24,11 @@ class Level:
         self.bg_rect = self.bg_image.get_rect()
         
         self.time_limit = time_limit    # seconds; -1 = infinity
-        self.max_blocks = max_blocks    # -1 = infinity
         self.min_vel = min_vel          # velocity blocks fall
         self.max_vel = max_vel
+
+        self.block_freq = block_freq
+        self.block_freq_inc = block_freq_inc
         
         # items possibly dropped
         # [coconuts, bananas, ...]
