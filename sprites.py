472c91bd9d55f9d7b47d219a8c49e5f546d8bfb6
jasontbradshaw <jasontbradshaw@f6f5931a-53a0-11de-b413-c5e99dc0a8e2>
2009-06-16 19:22:47 +0000

    generalized update functions (got trig? we do now!), made a sprite's speed and velocity independent of update for future use in interpolation

--- sprites.py
+++ sprites.py
@@ -1,27 +1,31 @@
 import pygame
+import math
 
 class Avatar(pygame.sprite.Sprite):
-    def __init__(self, surface, initial_position, vel = 0,
-            lives = 0, points = 0):
+    def __init__(self, surface, initial_position, dir = 0, vel = 0,
+                 lives = 0, points = 0):
         pygame.sprite.Sprite.__init__(self)
         self.image = surface
         self.rect = self.image.get_rect()
         self.rect.topleft = initial_position
+
+        self.dir = dir
         self.vel = vel
+        
         self.lives = lives
         self.points = points
 
-    def update(self, dir):
+    def update(self, dir = 0, vel = 0):
         """
-        If dir > 0, move avatar right by self.vel pixels.
-        If dir < 0, move avatar left.
+        dir = direction in degrees (right = 0, down = 90, left = 180, up = 270)
+        vel = speed in pixels per tick
         """
+        self.dir = dir
+        self.vel = vel
+        rad = math.radians(self.dir)
         
-        if dir > 0:
-            self.rect.move_ip(self.vel, 0)
-        else:
-            self.rect.move_ip(-self.vel, 0)
-
+        self.rect.move_ip(self.vel * math.cos(rad), self.vel * math.sin(rad))
+        
     def left_pos(self):
         return self.rect.left
 
@@ -30,30 +34,42 @@ class Avatar(pygame.sprite.Sprite):
 
 class Block(pygame.sprite.Sprite):
 
-    def __init__(self, surface, initial_position, vel = 0, ground_lvl = 0):
+    def __init__(self, surface, initial_position, dir = 0, vel = 0,
+                 ground_lvl = 0):
         pygame.sprite.Sprite.__init__(self)
         self.image = surface
         self.rect = self.image.get_rect()
         self.rect.topleft = initial_position
+        
+        self.dir = dir
         self.vel = vel
+        
         self.ground_lvl = ground_lvl
         self.time_hit_ground = 0
         self.timeout = 5000     # ms after on ground before killed
         self.hit_ground = False
         self.collided = False   # can only collide w/ avatar once
 
-    def update(self):
-        """moves block down by self.vel pixels"""
+    def update(self, dir = 0, vel = 0):
+        """
+        dir = direction in degrees (right = 0, down = 90, left = 180, up = 270)
+        vel = speed in pixels per tick
+        """
+        self.dir = dir
+        self.vel = vel
+        rad = math.radians(self.dir)
+        
         if not self.on_ground():
-            self.rect.move_ip(0, self.vel)
+            self.rect.move_ip(self.vel * math.cos(rad), self.vel * math.sin(rad))
         elif not self.hit_ground:   # has hit ground but flag not set
             # start timeout count down before killing sprite
             self.hit_ground = True
+            #self.vel = 0
             self.time_hit_ground = pygame.time.get_ticks()
         
         # kill sprite if timeout done
         if self.hit_ground and \
-                pygame.time.get_ticks() - self.time_hit_ground >= self.timeout:
+           pygame.time.get_ticks() - self.time_hit_ground >= self.timeout:
             self.kill()
 
     def on_ground(self):
