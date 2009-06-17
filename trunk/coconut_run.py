a6507f9fa48436bb560178fd223e25c230880cd2
jasontbradshaw <jasontbradshaw@f6f5931a-53a0-11de-b413-c5e99dc0a8e2>
2009-06-17 20:43:38 +0000

    abstracted sprites into an "Object" class to make things cleaner and more general

--- trunk/coconut_run.py
+++ trunk/coconut_run.py
@@ -15,10 +15,13 @@ def main(argv=None):
     if argv is None:
         argv = sys.argv
 
+    # resources
     resource_folder = 'resources/'
     fonts_folder = 'fonts/'
+    
     avatar_file = resource_folder + 'avatar.bmp'
     block_file = resource_folder + 'coconut.bmp'
+    
     COLORKEY = 0xFF00FF # transparent color
 
     # clock, for fps info and timing
@@ -27,21 +30,20 @@ def main(argv=None):
     # keyboard delay before key repeats
     pygame.key.set_repeat(10, 0)
 
-    # load level
+    # level
     lvl = Level("Cocoana", "landscape.bmp",
-            120, 0.015, 0.0001, 0.5,
+            120, 0.015, 0.0001, 0.75,
             10, 15,
             0, 0, width, height - 40)
 
-    # load and initialize avatar
+    # avatar
     avatar_surf = pygame.image.load(avatar_file).convert()
     avatar_surf.set_colorkey(COLORKEY)
     avatar_rect = avatar_surf.get_rect()
-    avatar_vel = 20
+    avatar_speed = 15
     avatar_lives = 3
-    avatar = Avatar(avatar_surf,
-                    [lvl.right/2, lvl.bottom - avatar_rect.height], 0,
-                    0, avatar_lives)
+    avatar = Avatar(avatar_surf, [lvl.right/2, lvl.bottom - avatar_rect.height],
+                    0, 0, avatar_speed, avatar_lives, 0)
 
     # blocks
     block_surf = pygame.image.load(block_file).convert()
@@ -52,17 +54,19 @@ def main(argv=None):
     default_font = pygame.font.Font(resource_folder + fonts_folder +
                                     "anmari.ttf", 26)
     COLOR_BLACK = (0, 0, 0)
+    
     fps_display_pos = (lvl.right - 140, 20)
     level_display_pos = (lvl.right / 2 - 50, 20)
     lives_display_pos = (20, 20)
     points_display_pos = (20, 50)
 
+    # menus
     full_screen_image(resource_folder + "main_menu.png")
     game_over = False
 
     # main loop
-    logic_fps = 30
-    skip_ticks = 1000 / logic_fps # ticks between logic updates
+    logic_fps = 3
+    skip_ticks = 1000 / logic_fps # ticks between logic updates in milliseconds
     max_frameskip = 5 # minimum fps the game will run at before slowing down
 
     next_logic_tick = pygame.time.get_ticks()
@@ -82,26 +86,28 @@ def main(argv=None):
             
             # input handling
             for event in pygame.event.get():
-                if event.type == pygame.QUIT: sys.exit()
+                if event.type == pygame.QUIT:
+                    sys.exit()
                 elif event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_LEFT:
+                    if event.key == pygame.K_ESCAPE:
+                        sys.exit()
+                    elif event.key == pygame.K_LEFT: # do these last!
                         if avatar.left_pos() > lvl.left:
-                            avatar.update(180, avatar_vel)
+                            avatar.move(180, avatar.speed)
                     elif event.key == pygame.K_RIGHT:
                         if avatar.right_pos() < lvl.right:
-                            avatar.update(0, avatar_vel)
-                    elif event.key == pygame.K_ESCAPE:
-                        sys.exit()
-                    #elif event.key != pygame.K_LEFT and \
-                         #event.key != pygame.K_RIGHT: # not being moved
-                        #avatar.update()
+                            avatar.move(0, avatar.speed)
+                elif event.type == pygame.KEYUP:
+                    if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                        avatar.vel = 0.0
 
             # block creation
             if random.random() < lvl.blk_freq:
+                block_speed = random.uniform(lvl.min_vel, lvl.max_vel)
                 blocks.add(Block(block_surf,
                                  [random.randint(lvl.left, lvl.right), lvl.top],
-                                 90, random.uniform(lvl.min_vel, lvl.max_vel),
-                                 lvl.bottom))
+                                 90, block_speed,
+                                 block_speed, lvl.bottom))
 
             # update game state
             avatar.points += 1
@@ -110,7 +116,7 @@ def main(argv=None):
 
             # update sprites
             for b in blocks:
-                b.update(b.dir, b.vel)
+                b.update()
 
             # collision detection
             collide_list = pygame.sprite.spritecollide(avatar, blocks, False)
@@ -137,14 +143,13 @@ def main(argv=None):
         delta = ((1.0 * pygame.time.get_ticks() + skip_ticks - next_logic_tick)
                  / skip_ticks)
         
-        # draw sprites
+        # draw sprites w/ delta movement
         screen.blit(lvl.bg_image, lvl.bg_rect)
+        
         for b in blocks:
-            fake_rect = b.rect.move(0, 0)
-            screen.blit(b.image, get_delta_coords(b, delta))
+            b.blit_delta(screen, delta)
 
-        fake_rect = avatar.rect.move(0, 0)
-        screen.blit(avatar.image, get_delta_coords(avatar, delta))
+        avatar.blit_delta(screen, delta)
 
         # draw text
         screen.blit(default_font.render(lvl.name, 1, COLOR_BLACK),
@@ -158,6 +163,7 @@ def main(argv=None):
         # draw debug text
         screen.blit(default_font.render('blk_freq: %f' % lvl.blk_freq, 1,
                                         COLOR_BLACK), (20, 80))
+        
         pygame.display.flip()
         
 
