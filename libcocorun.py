c489a9f95ddcb189ccde4e85bbe6b0734840195a
elbenshira <elbenshira@f6f5931a-53a0-11de-b413-c5e99dc0a8e2>
2009-07-17 04:23:19 +0000

    new expression engine

--- libcocorun.py
+++ libcocorun.py
@@ -1,8 +1,32 @@
 import random
+import math
 
-binary_ops = ['+', '-', '*', '/', '**', '%']
-unary_ops = ['++']
-operators = binary_ops #binary_ops & unary_ops
+binary_optrs = ['+', '-', '*', '/', '**', '%']
+unary_optrs = ['sin']
+operators = binary_optrs #binary_optrs & unary_ops
+
+class Op(str):
+    """
+    The Op class holds an operand or operator, depending on the content of the
+    string.
+    """
+
+    def oprnd(self):
+        return self not in operators
+        # TODO: use regular expressions so we can take decimal numbers
+        """for c in self:
+            if c < '0' or c > '9':
+                return False
+        return True"""
+
+    def optr(self):
+        return self in operators
+
+    def binary_optr(self):
+        return self in binary_optrs
+    
+    def unary_optr(self):
+        return self in unary_optrs
 
 class Expr(list):
     """
@@ -26,34 +50,30 @@ class Expr(list):
         """
         Returns True if self is valid.
 
-        self = [] is valid
-        self = [x] is valid iff x is valid
+        self = [] is invalid
         
-        for self = [x:xs]: for every e in self,
-       
-        e is a binary operator ==>
-            (e is not first or last element) and (left(e) and right(e) are
-            expressions)
-        e is a unary operator ==>
-            e is not last element and right(e) is an expression
-        e is an expression ==>
+        for every e in self:
+          e is expr ==>
             e is valid
-            e is first element ==> right(e) is binary operator
-            e is last element ==> left(e) is operator
-            e is not(first or last) ==> right(e) and left(e) are operators
-        
+            e first element ==> right(e) is binary optr
+            e last element ==> left(e) is optr
+            e not (first or last) element ==>
+              left(e) is optr and right(e) is binary optr
+          e is optr ==>
+            e is binary optr ==>
+              e not first or last
+              left(e) and right(e) is oprnd or valid expr
+            e is unary optr ==>
+              e not first ==> left(e) is optr
+              right(e) is oprnd or valid expr
+          e is oprnd ==>
+            e is first ==> right(e) is binary optr
+            e is last ==> left(e) is optr
+            e is not first or last ==> left(e) and right(e) is optr
         """
+
         if len(self) == 0:
             return False
-        if (len(self) == 1 and type(self[0]) == Expr and type((self[0])[0])
-            == str and (self[0])[0] not in operators):
-            # Expr consist of one Expr which is a number
-            return True
-        if type(self) is str:
-            # turtle all the way down to strings
-            if self in operators:
-                return False
-            return True
 
         for i in range(len(self)):
             e = self[i]
@@ -61,39 +81,64 @@ class Expr(list):
             right = None
 
             if i > 0:
-              left = self[i-1]
+                left = self[i-1]
             if i < len(self)-1:
-              right = self[i+1]
+                right = self[i+1]
 
-            if str(e) in binary_ops:
-                if (i == 0 or i == len(self)-1 or not left.valid() or not
-                right.valid()):
-                    # invalid binary operator
-                    return False
-            elif str(e) in unary_ops:
-                if i == len(self)-1 or not right(e).valid():
-                    # invalid unary operator
-                    return False
-            else: # an expression
-                if type(e) == str and len(e) == 1 and e not in operators:
-                    # a number
-                    continue
+            if type(e) == Expr:
                 if not e.valid():
                     return False
-                if i == 0 and str(right) not in binary_ops:
+                if i == 0 and not (type(right) == Op and right.binary_optr()):
                     return False
-                if i == len(self)-1 and str(left) not in operators:
+                if i == len(self)-1 and not (type(left) == Op and left.optr()):
                     return False
-                if (i != 0 and i != len(self)-1 and (str(left) not in
-                    operators or str(right) not in operators)):
+                if (i != 0 and i != len(self)-1 and not
+                    (type(left) == Op and type(right) == Op and
+                    left.optr() and right.binary_optr())):
                     return False
+            elif type(e) == Op and e.optr():
+                if e.binary_optr():
+                    if i == 0 or i == len(self)-1:
+                        return False
+                    if (not (((type(right) == Op and right.oprnd()) or
+                        (type(right) == Expr and right.valid())) and
+                        ((type(left) == Op and left.oprnd()) or
+                        (type(left) == Expr and left.valid())))):
+                        return False
+                elif e.unary_optr():
+                    if i == len(self)-1:
+                        return False
+                    if i > 0 and not ((type) == Op and left.optr()):
+                        return False
+                else:
+                    raise InvalidExpr("Expression Engine: operator not a\\
+                            binary or unary operator!")
+                    print "An operator not in binary and not in unary!"
+            elif type(e) == Op and e.oprnd():
+                if len(self) == 1:
+                    return True
+                if i == 0 and not (type(right) == Op and right.binary_optr()):
+                    return False
+                if i == len(self)-1 and not (type(left) == Op and left.optr()):
+                    return False
+                if (i != 0 and i != len(self)-1 and not (type(left) == Op and
+                    left.optr() and type(right) == Op and right.optr())):
+                    return False
+            else:
+                # screwed up!
+                raise InvalidExpr("Expression Engine: not an operator, \\
+                operand, or expression!")
         return True
 
     def __str__(self):
-        str = ""
+        # TODO: if unary operator, add ()'s around the next expression.
+        out = ""
         for e in self:
-            str += e.__str__()
-        return str
+            if type(e) == Expr:
+                out += "(" + str(e) + ")"
+            else:
+                out += str(e)
+        return out
 
 
 class InvalidExpr(Exception):
